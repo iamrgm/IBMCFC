a681b5258c7ae7694289ad453361a05411923ff3
Reuben George Mathew <mail4rgm@gmail.com>
2020-05-27 10:40:26 +0530

    Address Change

--- run/js/main.js
+++ run/js/main.js
@@ -1,30 +1,27 @@
-var form = document.getElementById('sendImg')
-var preloader = document.querySelector('.preloader')
-const url = "http://192.168.0.102:5000/test"
+var form = document.getElementById("sendImg");
+var preloader = document.querySelector(".preloader");
+const url = "http://localhost:5000/test";
 
+form.addEventListener("submit", (e) => {
+  preloader.style["display"] = "block";
+  e.preventDefault();
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
 
+  xhr.responseType = "blob";
+  xhr.onload = function (event) {
+    var blob = new Blob([this.response.value, { type: "audio/wav" }]);
+    const aurl = window.URL.createObjectURL(this.response);
+    console.log(aurl);
+    window.audio = new Audio();
+    window.audio.src = aurl;
+    preloader.style["display"] = "none";
+    Materialize.toast("Playing Audio", 3000, "rounded");
+    window.audio.play();
+  };
+  // or onerror, onabort
+  var formData = new FormData(form);
 
-form.addEventListener('submit',(e)=>{
-    preloader.style['display'] = 'block';
-    e.preventDefault()
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url); 
-
-    xhr.responseType = 'blob';
-    xhr.onload = function(event){ 
-        var blob = new Blob([this.response.value,{type: 'audio/wav'}])
-        const aurl = window.URL.createObjectURL(this.response);
-        console.log(aurl);
-        window.audio = new Audio();
-        window.audio.src = aurl;
-        preloader.style['display'] = 'none';
-        Materialize.toast('Playing Audio', 3000, 'rounded');
-        window.audio.play();   
-    }; 
-    // or onerror, onabort
-    var formData = new FormData(form); 
-    
-    xhr.send(formData);
-    console.log(formData.getAll('lang'));
-    
-})
+  xhr.send(formData);
+  console.log(formData.getAll("lang"));
+});
